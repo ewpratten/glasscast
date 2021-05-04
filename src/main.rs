3a426fce1d6a4473816f0199751e849e855c8ad0
Evan Pratten <ewpratten@gmail.com>
2021-05-04 08:51:08 -0400

    setting up data serialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::{App, Arg, value_t};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Wall {
+
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Light {
+
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct World {
+    walls: [Wall],
+    light: Light
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("glasscast")
+    .author("Evan Pratten <ewpratten@gmail.com>")
+    .arg(
+     Arg::with_name("world")
+        .takes_value(true)
+        .help("Path to the world JSON file")
+        .required(true)
+    )
+    .get_matches();
+
+    // Get data
+    let world = matches.value_of("world").unwrap();
+
+    // Parse the world
+
+    
 }
