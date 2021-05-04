4a1c69094d1c36175de9cb80a9b7ac09b70c8cf9
Evan Pratten <ewpratten@gmail.com>
2021-05-04 11:57:01 -0400

    finished renderer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::fs::File;
 
-use clap::{value_t, App, Arg};
+use clap::{App, Arg};
 use failure::Error;
+use geo::algorithm::euclidean_distance::EuclideanDistance;
+use geo::{Line, Point};
 use raylib::prelude::*;
 use raylib::{color::Color, math::Vector2};
 use serde::{Deserialize, Serialize};
@@ -18,8 +20,20 @@ struct Wall {
     #[serde(skip)]
     pub color: Color,
 
-    pub start: Vector2,
-    pub end: Vector2,
+    start: Vector2,
+    end: Vector2,
+
+    #[serde(skip)]
+    pub line: Option<Line<f32>>,
+}
+
+impl Wall {
+    fn load_line(&mut self) {
+        self.line = Some(Line::new(
+            Point::new(self.start.x, self.start.y),
+            Point::new(self.end.x, self.end.y),
+        ));
+    }
 }
 
 impl ColorLoad for Wall {
@@ -57,6 +71,7 @@ impl ColorLoad for World {
     fn load_colors(&mut self) {
         for wall in self.walls.iter_mut() {
             wall.load_colors();
+            wall.load_line();
         }
         self.light.load_colors();
     }
@@ -70,44 +85,20 @@ impl World {
     }
 }
 
-fn find_intersect(
-    wall_start: Vector2,
-    wall_end: Vector2,
-    ray_start: Vector2,
-    ray_end: Vector2,
-) -> Option<Vector2> {
-    let a1 = wall_end.y - wall_start.y;
-    let b1 = wall_start.x - wall_end.x;
-    let c1 = a1 * wall_start.x + b1 * wall_start.y;
-
-    let a2 = ray_end.y - ray_start.y;
-    let b2 = ray_start.x - ray_end.x;
-    let c2 = a2 * ray_start.x + b2 * ray_start.y;
-
-    let delta = a1 * b2 - a2 * b1;
-
-    if delta == 0.0 {
-        return None;
-    }
-
-    Some(Vector2 {
-        x: (b2 * c1 - b1 * c2) / delta,
-        y: (a1 * c2 - a2 * c1) / delta,
-    })
+fn find_intersect(wall: &Wall, point: Vector2) -> bool {
+    // Define the line
+    return wall
+        .line
+        .unwrap()
+        .euclidean_distance(&Point::new(point.x, point.y))
+        < 1.0;
 }
 
 fn get_color_modifier_of_pixel(pixel: Vector2, world: &World) -> Color {
     // Search all walls
     for wall in world.walls.iter() {
         // Check for collision
-        if find_intersect(
-            wall.start,
-            wall.end,
-            pixel - Vector2::new(0.5, 0.5),
-            pixel + Vector2::new(0.5, 0.5),
-        )
-        .is_some()
-        {
+        if find_intersect(&wall, pixel) {
             return wall.color;
         }
     }
@@ -126,7 +117,7 @@ fn plot(
     d: &mut RaylibDrawHandle,
 ) -> Option<Color> {
     // Calculate the current pixel coord
-    let pixel = (normal * magnitude) + *position;
+    let pixel = (normal * magnitude) + (*position * *window_vec);
 
     // We cannot plot outside the window
     if (pixel.x < 0.0 || pixel.x > window_vec.x) || (pixel.y < 0.0 || pixel.y > window_vec.y) {
@@ -136,17 +127,17 @@ fn plot(
     // Modify the light ray color
     let modifier = get_color_modifier_of_pixel(pixel, world);
     let ray_color = Color {
-        r: ray_color.r - modifier.r,
-        g: ray_color.g - modifier.g,
-        b: ray_color.b - modifier.b,
+        r: (ray_color.r as f32 - modifier.r as f32).clamp(u8::MIN as f32, u8::MAX as f32) as u8,
+        g: (ray_color.g as f32 - modifier.g as f32).clamp(u8::MIN as f32, u8::MAX as f32) as u8,
+        b: (ray_color.b as f32 - modifier.b as f32).clamp(u8::MIN as f32, u8::MAX as f32) as u8,
         a: 255,
     };
 
     // Plot the ray
     d.draw_pixel_v(
         Vector2 {
-            x: pixel.x * window_vec.x,
-            y: pixel.y * window_vec.y,
+            x: pixel.x,
+            y: pixel.y,
         },
         ray_color,
     );
@@ -159,12 +150,12 @@ fn trace_and_plot(
     position: &Vector2,
     normal: Vector2,
     window_vec: &Vector2,
-    ray_color: &Color,
+    ray_color: Color,
     world: &World,
     d: &mut RaylibDrawHandle,
 ) {
     let mut magnitude = 0.0;
-    let mut color = *ray_color;
+    let mut color = ray_color;
     loop {
         let new_color = plot(position, normal, magnitude, window_vec, &color, world, d);
         magnitude += 1.0;
@@ -197,15 +188,18 @@ fn main() {
 
     // Configure a window
     let (mut rl, thread) = raylib::init()
-        .size(1080, 720)
+        .size(800, 600)
         .title("GlassCast")
-        .msaa_4x()
+        // .msaa_4x()
         .vsync()
         .build();
 
+    // Load bloom shader
+    let bloom_shader = rl.load_shader(&thread, None, Some("./bloom.fs")).unwrap();
+    let bloom_surface = rl.load_render_texture(&thread, 800, 600).unwrap();
+
     while !rl.window_should_close() {
         let mut d = rl.begin_drawing(&thread);
-        d.clear_background(Color::WHITE);
 
         // Get the window size as a vector
         let window_vec = Vector2 {
@@ -222,24 +216,54 @@ fn main() {
             world.light.position = mouse_pos / window_vec;
         }
 
-        // Render every ray extending from the light
-        for angle in 0..180 {
-            let angle = angle as f32;
+        // Open a shader context
+        {
+            unsafe {
+                raylib::ffi::BeginTextureMode(*bloom_surface);
+            }
+            d.clear_background(Color::WHITE);
 
-            // Calculate the ray normal
-            let normal = Vector2 {
-                x: angle.to_radians().cos(),
-                y: angle.to_radians().sin(),
-            };
+            // Render every ray extending from the light
+            for angle in 0..360 {
+                let angle = angle as f32;
 
-            // Recursive render
-            trace_and_plot(
-                &world.light.position,
-                normal,
-                &window_vec,
-                &world.light.color,
-                &world,
-                &mut d,
+                // Calculate the ray normal
+                let normal = Vector2 {
+                    x: angle.to_radians().cos(),
+                    y: angle.to_radians().sin(),
+                };
+
+                // Recursive render
+                trace_and_plot(
+                    &world.light.position,
+                    normal,
+                    &window_vec,
+                    world.light.color,
+                    &world,
+                    &mut d,
+                );
+            }
+
+            unsafe {
+                raylib::ffi::EndTextureMode();
+            }
+        }
+
+        // Render via the shader
+        {
+            let mut shader_context = d.begin_shader_mode(&bloom_shader);
+
+            // Blit the texture
+            shader_context.draw_texture_rec(
+                &bloom_surface,
+                Rectangle {
+                    x: 0.0,
+                    y: 0.0,
+                    width: bloom_surface.width() as f32,
+                    height: (bloom_surface.height() as f32) * -1.0,
+                },
+                Vector2::zero(),
+                Color::WHITE,
             );
         }
 
