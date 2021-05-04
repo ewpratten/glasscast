f35d98cb343fcc60e9d94898ae1ab8cdabc58b86
Evan Pratten <ewpratten@gmail.com>
2021-05-04 10:01:07 -0400

    working on color casts

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use std::fs::File;
 
 use clap::{value_t, App, Arg};
 use failure::Error;
-use line_intersection::LineInterval;
 use raylib::prelude::*;
 use raylib::{color::Color, math::Vector2};
 use serde::{Deserialize, Serialize};
@@ -71,52 +70,112 @@ impl World {
     }
 }
 
-fn get_color_modifier_of_pixel(pixel: Vector2, world: &World) -> Color {
+fn find_intersect(
+    wall_start: Vector2,
+    wall_end: Vector2,
+    ray_start: Vector2,
+    ray_end: Vector2,
+) -> Option<Vector2> {
+    let a1 = wall_end.y - wall_start.y;
+    let b1 = wall_start.x - wall_end.x;
+    let c1 = a1 * wall_start.x + b1 * wall_start.y;
 
+    let a2 = ray_end.y - ray_start.y;
+    let b2 = ray_start.x - ray_end.x;
+    let c2 = a2 * ray_start.x + b2 * ray_start.y;
+
+    let delta = a1 * b2 - a2 * b1;
+
+    if delta == 0.0 {
+        return None;
+    }
+
+    Some(Vector2 {
+        x: (b2 * c1 - b1 * c2) / delta,
+        y: (a1 * c2 - a2 * c1) / delta,
+    })
+}
+
+fn get_color_modifier_of_pixel(pixel: Vector2, world: &World) -> Color {
     // Search all walls
     for wall in world.walls.iter() {
-
-        // Define the line segment of the wall
-        let segment = LineInterval::line_segment(Line {
-            start: (wall.start.x, wall.start.y).into(),
-            end: (wall.end.x, wall.end.y).into(),
-        });
-
+        // Check for collision
+        if find_intersect(
+            wall.start,
+            wall.end,
+            pixel - Vector2::new(0.5, 0.5),
+            pixel + Vector2::new(0.5, 0.5),
+        )
+        .is_some()
+        {
+            return wall.color;
+        }
     }
 
     // No modifier
     return Color::BLACK;
-
 }
 
-fn trace_and_plot(
+fn plot(
+    position: &Vector2,
     normal: Vector2,
     magnitude: f32,
     window_vec: &Vector2,
-    ray_color: &mut Color,
+    ray_color: &Color,
     world: &World,
     d: &mut RaylibDrawHandle,
-) {
+) -> Option<Color> {
     // Calculate the current pixel coord
-    let pixel = normal * magnitude;
-    let pixel = Vector2 {
-        x: pixel.x.floor(),
-        y: pixel.y.floor()
-    };
+    let pixel = (normal * magnitude) + *position;
 
     // We cannot plot outside the window
     if (pixel.x < 0.0 || pixel.x > window_vec.x) || (pixel.y < 0.0 || pixel.y > window_vec.y) {
-        return;
+        return None;
     }
 
     // Modify the light ray color
+    let modifier = get_color_modifier_of_pixel(pixel, world);
+    let ray_color = Color {
+        r: ray_color.r - modifier.r,
+        g: ray_color.g - modifier.g,
+        b: ray_color.b - modifier.b,
+        a: 255,
+    };
 
     // Plot the ray
-    d.draw_pixel_v(pixel, *ray_color);
+    d.draw_pixel_v(
+        Vector2 {
+            x: pixel.x * window_vec.x,
+            y: pixel.y * window_vec.y,
+        },
+        ray_color,
+    );
 
     // Iterate a step down the ray
-    trace_and_plot(normal, magnitude + 1.0, window_vec, ray_color, world, d);
+    return Some(ray_color);
+}
 
+fn trace_and_plot(
+    position: &Vector2,
+    normal: Vector2,
+    window_vec: &Vector2,
+    ray_color: &Color,
+    world: &World,
+    d: &mut RaylibDrawHandle,
+) {
+    let mut magnitude = 0.0;
+    let mut color = *ray_color;
+    loop {
+        let new_color = plot(position, normal, magnitude, window_vec, &color, world, d);
+        magnitude += 1.0;
+
+        // Handle edge of the screen
+        if new_color.is_none() {
+            return;
+        }
+
+        color = new_color.unwrap();
+    }
 }
 
 fn main() {
@@ -161,6 +220,27 @@ fn main() {
 
             // Normalize and set
             world.light.position = mouse_pos / window_vec;
+        }
+
+        // Render every ray extending from the light
+        for angle in 0..180 {
+            let angle = angle as f32;
+
+            // Calculate the ray normal
+            let normal = Vector2 {
+                x: angle.to_radians().cos(),
+                y: angle.to_radians().sin(),
+            };
+
+            // Recursive render
+            trace_and_plot(
+                &world.light.position,
+                normal,
+                &window_vec,
+                &world.light.color,
+                &world,
+                &mut d,
+            );
         }
 
         // Render FPS counter
